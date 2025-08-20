e86f4f355ff99ece50a1095fbe9d92687725026b
Saravanan A <116100176+Saravananarunachalam@users.noreply.github.com>
2025-08-20 20:09:50 +0530

    Update Patient.java

--- src/main/java/com/example/demo/model/Patient.java
+++ src/main/java/com/example/demo/model/Patient.java
@@ -13,60 +13,95 @@ public class Patient {
     private String name;
     private int age;
     private String gender;
+    private double weight;
     private String contactNumber;
+    private String address;
+   
     @OneToOne
     @JoinColumn(name = "room_id")
     private Room room;
-    
-    
+
 	public Patient() {
 		super();
 	}
-	public Patient(Long id, String name, int age, String gender, String contactNumber, Room room) {
+
+	public Patient(Long id, String name, int age, String gender, double weight, String contactNumber, String address,
+			Room room) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.age = age;
 		this.gender = gender;
+		this.weight = weight;
 		this.contactNumber = contactNumber;
+		this.address = address;
 		this.room = room;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
 	public String getContactNumber() {
 		return contactNumber;
 	}
+
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	public Room getRoom() {
 		return room;
 	}
+
 	public void setRoom(Room room) {
 		this.room = room;
 	}
     
+ 
         
 }
