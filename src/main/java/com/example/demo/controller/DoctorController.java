be9e61c6c1fb94b5298ac001dd515cf1b728d061
Saravanan A <116100176+Saravananarunachalam@users.noreply.github.com>
2025-08-20 19:58:51 +0530

    Update DoctorController.java

--- src/main/java/com/example/demo/controller/DoctorController.java
+++ src/main/java/com/example/demo/controller/DoctorController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-//import org.springframework.security.access.prepost.PreAuthorize;
 import com.example.demo.model.Doctor;
 import com.example.demo.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,35 +15,35 @@ public class DoctorController {
     private DoctorService doctorService;
 
     @GetMapping
-  //  @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String listDoctors(Model model) {
         model.addAttribute("doctors", doctorService.findAll());
         return "doctors/list";
     }
 
     @GetMapping("/new")
-  //  @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String showFormForAdd(Model model) {
         model.addAttribute("doctor", new Doctor());
         return "doctors/form";
     }
 
     @GetMapping("/edit/{id}")
-   // @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String showFormForUpdate(@PathVariable Long id, Model model) {
         doctorService.findById(id).ifPresent(doctor -> model.addAttribute("doctor", doctor));
         return "doctors/form";
     }
 
     @PostMapping("/save")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String saveDoctor(@ModelAttribute("doctor") Doctor doctor) {
         doctorService.save(doctor);
         return "redirect:/doctors";
     }
 
     @GetMapping("/delete/{id}")
-  //  @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteDoctor(@PathVariable Long id) {
         doctorService.deleteById(id);
         return "redirect:/doctors";
