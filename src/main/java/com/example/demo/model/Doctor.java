b261dbe9ffebc4ec0e1444aa0f46b406458c9d5a
Saravanan A <116100176+Saravananarunachalam@users.noreply.github.com>
2025-08-20 20:08:25 +0530

    Update Doctor.java

--- src/main/java/com/example/demo/model/Doctor.java
+++ src/main/java/com/example/demo/model/Doctor.java
@@ -11,18 +11,27 @@ public class Doctor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String gender;
     private String specialty;
+    private int experience;
     private String contactNumber;
+    @OneToOne(mappedBy = "doctor")
+    private Users user; // Link to User entity for authentication
+    
     
 	public Doctor() {
 		super();
 	}
-	public Doctor(Long id, String name, String specialty, String contactNumber) {
+	public Doctor(Long id, String name, String gender, String specialty, int experience, String contactNumber,
+			Users user) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.gender = gender;
 		this.specialty = specialty;
+		this.experience = experience;
 		this.contactNumber = contactNumber;
+		this.user = user;
 	}
 	public Long getId() {
 		return id;
@@ -36,11 +45,23 @@ public class Doctor {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 	public String getSpecialty() {
 		return specialty;
 	}
 	public void setSpecialty(String specialty) {
 		this.specialty = specialty;
+	}
+	public int getExperience() {
+		return experience;
+	}
+	public void setExperience(int experience) {
+		this.experience = experience;
 	}
 	public String getContactNumber() {
 		return contactNumber;
@@ -48,6 +69,13 @@ public class Doctor {
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
+	public Users getUser() {
+		return user;
+	}
+	public void setUser(Users user) {
+		this.user = user;
+	}
     
     
+	    
 }
