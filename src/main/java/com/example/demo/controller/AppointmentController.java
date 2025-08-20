e371e12890e5f661b9415d8dfd391cc99984daea
Saravanan A <116100176+Saravananarunachalam@users.noreply.github.com>
2025-08-20 19:57:38 +0530

    Update AppointmentController.java

--- src/main/java/com/example/demo/controller/AppointmentController.java
+++ src/main/java/com/example/demo/controller/AppointmentController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Appointment;
-//import org.springframework.security.access.prepost.PreAuthorize;
 import com.example.demo.service.AppointmentService;
 import com.example.demo.service.DoctorService;
 import com.example.demo.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +21,14 @@ public class AppointmentController {
     private DoctorService doctorService;
 
     @GetMapping
-   // @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
     public String listAppointments(Model model) {
         model.addAttribute("appointments", appointmentService.findAll());
         return "appointments/list";
     }
 
     @GetMapping("/new")
-    //@PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
     public String showFormForAdd(Model model) {
         model.addAttribute("appointment", new Appointment());
         model.addAttribute("patients", patientService.findAll());
@@ -37,7 +37,7 @@ public class AppointmentController {
     }
 
     @GetMapping("/edit/{id}")
-   // @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
     public String showFormForUpdate(@PathVariable Long id, Model model) {
         appointmentService.findById(id).ifPresent(appointment -> model.addAttribute("appointment", appointment));
         model.addAttribute("patients", patientService.findAll());
@@ -46,14 +46,14 @@ public class AppointmentController {
     }
 
     @PostMapping("/save")
-   // @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
     public String saveAppointment(@ModelAttribute("appointment") Appointment appointment) {
         appointmentService.save(appointment);
         return "redirect:/appointments";
     }
 
     @GetMapping("/delete/{id}")
-  //  @PreAuthorize("hasAnyRole('ADMIN', 'RECEPTIONIST')")
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteAppointment(@PathVariable Long id) {
         appointmentService.deleteById(id);
         return "redirect:/appointments";
