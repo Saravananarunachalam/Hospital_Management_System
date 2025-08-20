0efa787522bef46fe533f6708188b9435508acee
Saravanan A <116100176+Saravananarunachalam@users.noreply.github.com>
2025-08-20 20:23:10 +0530

    Create Database.sql

---
+++ Database.sql
@@ -0,0 +1,13 @@
+create database Hospital_Management;
+use Hospital_Management;
+
+drop database Hospital_Management;
+show tables;
+show databases;
+select * from patient;
+select * from doctor;
+select * from appointment;
+select * from room;
+select * from users;
+
+
